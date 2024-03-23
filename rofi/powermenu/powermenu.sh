918723892f05d60f28d6e828ae2079ac184e8883
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2024-03-22 23:44:09 -0300

    First commit

--- rofi/powermenu/powermenu.sh
+++ rofi/powermenu/powermenu.sh
@@ -85,10 +85,8 @@ case ${chosen} in
 		run_cmd --reboot
         ;;
     $lock)
-		if [[ -x '/usr/local/bin/betterlockscreen' ]]; then
+		if [[ -x '/usr/bin/betterlockscreen' ]]; then
 			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
 		fi
         ;;
     $suspend)
